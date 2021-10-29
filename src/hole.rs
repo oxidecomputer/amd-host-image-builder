e86b542bbe47463a1a1e0e28253b4ed07d39f5d7
Keith M Wesolowski <60712355+wesolows@users.noreply.github.com>
2021-10-28 18:55:48 -0700

    main: Add simple ELF interpreter for loader programs.

---
+++ src/hole.rs
@@ -0,0 +1,28 @@
+use std::io::Read;
+use std::cmp::min;
+
+pub struct Hole {
+    cursor: usize // amount remaining
+}
+
+impl Hole {
+    pub fn new(sz: usize) -> Self {
+        Self {
+            cursor: sz
+        }
+    }
+}
+
+impl Read for Hole {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        if (self.cursor == 0) {
+            return Ok(0)
+        }
+        let l = min(buf.len(), self.cursor);
+        for slot in &mut buf[0 .. l] {
+            *slot = 0;
+        }
+        self.cursor -= l;
+        Ok(l)
+    }
+}
