7c444aa39d31e9011d1b180d700ab5dd9d2c2e48
Danny Milosavljevic <daym@oxidecomputer.com>
2022-05-27 21:59:54 +0200

    build: Validate etc/Milan.json against the JSON schema we generate.

--- build/main.rs
+++ build/main.rs
@@ -3,6 +3,7 @@ mod config_schema;
 use std::env;
 use std::fs::{self};
 use std::process;
+use jsonschema::JSONSchema;
 
 fn main() {
 	// OUT_DIR is set by Cargo and it's where any additional build artifacts
@@ -16,4 +17,22 @@ fn main() {
 	};
 	fs::create_dir_all(&out_dir).unwrap();
 	config_schema::generate_config_json_schema(&out_dir);
+
+	// Make sure at least our default config in etc/Milan.json validates
+	// using the schema we just generated.
+	let schema_filename = format!("{}/{}", out_dir.into_string().unwrap(), "efs.schema.json");
+	let schema_str = std::fs::read_to_string(schema_filename).unwrap();
+	let schema_json: serde_json::Value = serde_json::from_str(&schema_str).unwrap();
+	let schema_validator = JSONSchema::compile(&schema_json).unwrap();
+
+	let configuration_filename = "etc/Milan.json";
+	let configuration_str = std::fs::read_to_string(configuration_filename).unwrap();
+	let configuration_json: serde_json::Value = serde_json::from_str(&configuration_str).unwrap();
+
+	if let Err(errors) = schema_validator.validate(&configuration_json) {
+		for error in errors {
+			eprintln!("validation error: {:#?}", error);
+		}
+		std::process::exit(2);
+	};
 }
