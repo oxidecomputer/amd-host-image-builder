97699259d283ba9506934e2a57db5d30f3c00538
Danny Milosavljevic <daym@oxidecomputer.com>
2022-06-15 22:01:44 +0200

    tests: Make the test find the efs schema.

--- tests/schema.rs
+++ tests/schema.rs
@@ -1,19 +1,15 @@
 use std::path::Path;
 use valico::json_schema;
 
+const SCHEMA_STR: &str = include_str!(concat!(env!("OUT_DIR"), "/efs.schema.json"));
+
 #[test]
 fn test_schema() {
 	// Make sure at least our default config in etc/Milan.json validates
 	// using the schema we just generated.
-	let out_dir = match std::env::var_os("OUT_DIR") {
-		Some(out_dir) => out_dir,
-		None => {
-			panic!("OUT_DIR environment variable not defined.");
-		}
-	};
-	let schema_filename = Path::new(&out_dir).join("efs.schema.json");
-	let schema_str = std::fs::read_to_string(schema_filename).unwrap();
-	let schema_json: serde_json::Value = serde_json::from_str(&schema_str).unwrap();
+	//let schema_filename = Path::new(&out_dir).join("efs.schema.json");
+	//let schema_str = std::fs::read_to_string(schema_filename).unwrap();
+	let schema_json: serde_json::Value = serde_json::from_str(SCHEMA_STR).unwrap();
 	let configuration_filename = Path::new("etc").join("Milan.efs.json5");
 	let configuration_str = std::fs::read_to_string(configuration_filename).unwrap();
 	let configuration_json: serde_json::Value = json5::from_str(&configuration_str).unwrap();
