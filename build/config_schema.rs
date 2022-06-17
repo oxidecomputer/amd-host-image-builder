412f471df848f2d6b09b4ad201918f2f744fdccd
Danny Milosavljevic <daym@oxidecomputer.com>
2022-06-17 22:28:19 +0200

    build: Work around schemars issue #62.

--- build/config_schema.rs
+++ build/config_schema.rs
@@ -1,10 +1,19 @@
 use amd_host_image_builder_config::SerdeConfig;
+use schemars::gen::SchemaSettings;
 use std::ffi::OsString;
 use std::fs;
 use std::path::Path;
 
 pub fn generate_config_json_schema(outdir: &OsString) {
-	let schema = schemars::schema_for!(SerdeConfig);
+	let settings = SchemaSettings::default().with(|s| {
+		// Work around schemars issue #62.
+		// Downside: This makes the schema bigger by an order
+		// of magnitude.
+		s.inline_subschemas = true
+	});
+	let gen = settings.into_generator();
+	let schema = gen.into_root_schema_for::<SerdeConfig>();
+
 	let schema_file = Path::new(outdir).join("efs.schema.json");
 	fs::write(schema_file, serde_json::to_string_pretty(&schema).unwrap())
 		.unwrap();
