858b4bc12ff4d40b2ff85529119d407f928a81d4
Danny Milosavljevic <daym@oxidecomputer.com>
2022-08-16 21:10:03 +0200

    build: Make rebuilds less frequent.
    Fixes <https://github.com/oxidecomputer/amd-host-image-builder/issues/68>.

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::process;
+
 use amd_host_image_builder_config::SerdeConfig;
 use schemars::gen::SchemaSettings;
 use std::ffi::OsString;
@@ -17,4 +20,17 @@ pub fn generate_config_json_schema(outdir: &OsString) {
 	let schema_file = Path::new(outdir).join("efs.schema.json");
 	fs::write(schema_file, serde_json::to_string_pretty(&schema).unwrap())
 		.unwrap();
+}
+
+fn main() {
+	// OUT_DIR is set by Cargo and it's where any additional build artifacts
+	// are written.
+	let out_dir = match env::var_os("OUT_DIR") {
+		Some(out_dir) => out_dir,
+		None => {
+			eprintln!("OUT_DIR environment variable not defined.");
+			process::exit(1);
+		}
+	};
+	generate_config_json_schema(&out_dir);
 }
