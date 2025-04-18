e67e9539e79a50440998d6a421949490e8e5ef72
Dan Cross <cross@oxidecomputer.com>
2025-04-18 02:24:13 +0000

    ci: configure buildomat to use our rust-toolchain
    Signed-off-by: Dan Cross <cross@oxidecomputer.com>

--- .github/buildomat/jobs/build-and-test.sh
+++ .github/buildomat/jobs/build-and-test.sh
@@ -3,7 +3,7 @@
 #: name = "build-and-test"
 #: variety = "basic"
 #: target = "helios"
-#: rust_toolchain = "nightly-2021-09-01"
+#: rust_toolchain = "stable"
 #: output_rules = [
 #:	"/work/bins/*",
 #: ]
@@ -20,33 +20,11 @@ set -o errexit
 set -o pipefail
 set -o xtrace
 
+export LD=gld
+
+gld --version
 rustc --version
 cargo --version
-
-#
-# The token authentication mechanism that affords us access to other private
-# repositories requires that we use HTTPS URLs for GitHub, rather than SSH.
-#
-override_urls=(
-    'git://github.com/'
-    'git@github.com:'
-    'ssh://github.com/'
-    'ssh://git@github.com/'
-)
-for (( i = 0; i < ${#override_urls[@]}; i++ )); do
-	git config --add --global url.https://github.com/.insteadOf \
-	    "${override_urls[$i]}"
-done
-
-#
-# Require that cargo use the git CLI instead of the built-in support.  This
-# achieves two things: first, SSH URLs should be transformed on fetch without
-# requiring Cargo.toml rewriting, which is especially difficult in transitive
-# dependencies; second, Cargo does not seem willing on its own to look in
-# ~/.netrc and find the temporary token that buildomat generates for our job,
-# so we must use git which uses curl.
-#
-export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
 banner test
 ptime -m cargo xtask test --verbose --locked
